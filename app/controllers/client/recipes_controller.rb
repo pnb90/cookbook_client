1f3ef525f4064e502543b6f5237f778d2e6ed95d
pnb90 <phuocbui90@gmail.com>
2019-02-07 12:45:03 -0600

    added new route

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -5,6 +5,29 @@ class Client::RecipesController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client_params = {
+                     title: params[:title], 
+                     chef: params[:chef],
+                     prep_time: params[:prep_time], 
+                     ingredients: params[:ingredients], 
+                     directions: params[:directions], 
+                     image_url: params[:ingredients] 
+                     }
+
+    response = HTTP.post(
+                        "http://localhost:3000/api/recipes", 
+                        form: client_params
+                        )
+
+    recipe = response.parse
+    redirect_to "/client/recipes/#{ recipe["id"] }"
+  end
+
   def show
     # @recipe = Recipe.find(params[:id])
 
@@ -12,4 +35,5 @@ class Client::RecipesController < ApplicationController
     @recipe = response.parse
     render 'show.html.erb'
   end
+
 end
